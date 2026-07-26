79bfebfd600e19d48012a04137a76b90832f2156
Draglil <minysinsk2016@bk.ru>
2026-07-26 19:56:09 +0700

    feat: create navigation for Login and Signin

--- android/app/src/main/java/com/polka/android/presentation/navigation/AuthNavGraph.kt
+++ android/app/src/main/java/com/polka/android/presentation/navigation/AuthNavGraph.kt
@@ -1,5 +1,56 @@
 package com.polka.android.presentation.navigation
 
-fun NavGraphBuilder.authNavGraph {
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+import androidx.navigation.navigation
+import com.polka.android.presentation.authSys.LoginScreen
+import com.polka.android.presentation.authSys.SigninScreen
 
+fun NavGraphBuilder.authNavGraph (
+    navController: NavHostController
+) {
+    navigation(
+        startDestination = Destination.Login.route,
+        route = "auth"
+    ){
+        composable(
+            route = Destination.Login.route,
+            arguments = listOf(
+                navArgument("showSigninSuccess"){ defaultValue = false }
+            )
+        ){ backStackEntry ->
+            val showSuccess = backStackEntry.arguments?.getBoolean("showSigninSuccess") ?: false
+
+            LoginScreen(
+                showSigninSuccess = showSuccess,
+                onSigninClick = {
+                    navController.navigate(Destination.Signin.route)
+                },
+                onSuccess = { userId ->
+                    navController.navigate(
+                        Destination.getNextDestinationAfterLogin(userId)
+                    ){
+                        popUpTo(Destination.Login.route){ inclusive = true }
+                    }
+                }
+            )
+        }
+
+        composable(Destination.Signin.route){
+            SigninScreen(
+                onLoginClick = {
+                    navController.navigate(Destination.Login.pass(showSigninSuccess = false)){
+                        popUpTo(Destination.Signin.route){ inclusive = true }
+                    }
+                },
+                onSigninSuccess = {
+                    navController.navigate(Destination.Login.pass(showSigninSuccess = true)){
+                        popUpTo(Destination.Signin.route){ inclusive = true }
+                    }
+                }
+            )
+        }
+    }
 }
