512fea14b85c7a75e1042af07581ae6f5f504252
Draglil <65397329+Draglil@users.noreply.github.com>
2026-07-30 14:58:04 +0700

    feat: add colors, update application theme

--- android/app/src/main/java/com/polka/android/presentation/theme/Color.kt
+++ android/app/src/main/java/com/polka/android/presentation/theme/Color.kt
@@ -1,4 +1,65 @@
 package com.polka.android.presentation.theme
 
-class Color {
-}
+import androidx.compose.ui.graphics.Color
+
+// ===== Main colors =====
+val Primary = Color(0xFF9B5860)
+val OnPrimary = Color(0xFFFFFFFF)
+val PrimaryContainer = Color(0xFFEADDFF) // -
+val OnPrimaryContainer = Color(0xFF21005D) // -
+
+// ===== Secondary colors =====
+val Secondary = Color(0xFF625B71) // -
+val OnSecondary = Color(0xFFFFFFFF) // -
+val SecondaryContainer = Color(0xFFE8DEF8) // -
+val OnSecondaryContainer = Color(0xFF1D192B) // -
+
+// ===== Tertiary colors =====
+val Tertiary = Color(0xFF7D5260) // -
+val OnTertiary = Color(0xFFFFFFFF) // -
+val TertiaryContainer = Color(0xFFFFD8E4) // -
+val OnTertiaryContainer = Color(0xFF31111D) // -
+
+// ===== Background =====
+val Background = Color(0xFFFFFBFE) // -
+val OnBackground = Color(0xFF1C1B1F) // -
+val Surface = Color(0xFF484D6D) // TIP: tiles and input container background
+val OnSurface = Color(0xFFF5EFF7)  // TIP: text on Surface
+val SurfaceVariant = Color(0xFFECE6F0) // TIP: search bar background
+val OnSurfaceVariant = Color(0xFF49454F) // TIP: text on SurfaceVariant
+
+// ===== Errors =====
+val Error = Color(0xFFBA1A1A) // -
+val OnError = Color(0xFFFFFFFF) // -
+val ErrorContainer = Color(0xFFFFDAD6) // -
+val OnErrorContainer = Color(0xFF410002) // -
+
+// ===== Outline / stroke =====
+val Outline = Surface
+val OutlineVariant = Color(0xFFCAC4D0) // -
+
+// ===== Custom colors =====
+val PolkaOne = Color(0xFF08B2E3)
+val PolkaTwo = Color(0xFF30ADAB)
+val PolkaThree = Color(0xFF57A773)
+val PolkaFour = Color(0xFFEFE9F4)
+val PolkaFive = Color(0xFFFFECD8)
+val PolkaSix = Color(0xFFEE6352)
+val PolkaSeven = Color(0xFFBB8F98)
+val PolkaEight = Color(0xFF507A70)
+val PolkaNine = Color(0xFF9B5860)
+val PolkaTen = Color(0xFF484D6D)
+val PolkaEleven = Color(0xFF1CB0C7)
+val PolkaTwelwe = Color(0xFF44AA8F)
+
+val PolkaButtonFriends = PolkaEleven
+val PolkaButtonAccount = PolkaTwo
+val PolkaButtonRecommendations = PolkaTwelwe
+val PolkaButtonOverview = PolkaThree
+val PolkaButtonSettings = PolkaOne
+val PolkaButtonSupport = PolkaSeven
+val PolkaButtonLogOut = PolkaSix
+val PolkaButtonTip = PolkaEight
+val PolkaButtonCancel = PolkaSix
+val PolkaButtonCurrentItem = PolkaThree
+val PolkaButtonSupportProject = PolkaSeven
