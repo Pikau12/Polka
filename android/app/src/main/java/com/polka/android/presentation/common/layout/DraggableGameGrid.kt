13a2e99f31c771579a85f83e7e114cabe6dee6c5
Draglil <65397329+Draglil@users.noreply.github.com>
2026-08-07 12:22:41 +0700

    fix: update DraggableGameGrid

--- android/app/src/main/java/com/polka/android/presentation/common/layout/DraggableGameGrid.kt
+++ android/app/src/main/java/com/polka/android/presentation/common/layout/DraggableGameGrid.kt
@@ -1,22 +1,15 @@
 package com.polka.android.presentation.common.layout
 
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
-import com.polka.android.data.model.CollectionItem
+import com.polka.android.presentation.model.CollectionItem
+import com.polka.android.presentation.common.tiles.ContextMenuAction
 
 @Composable
 fun DraggableGameGrid(
     collection: List<CollectionItem>,
-
+    onDoubleClick: (Long) -> Unit,
+    onContextMenu: (Long, ContextMenuAction) -> Unit,
+    onAddGameClick: () -> Unit,
 ){
 
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(3)
-    ) {
-        items(collection) { collectionItem ->
-
-        }
-    }
 }
