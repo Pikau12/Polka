525aed46b7df2c3173e6535915114368f26532d6
vilmo <morgraphvillain@gmail.com>
2026-08-14 00:13:15 +0500

    feat: update collection item (entity and model)

--- android/app/src/main/java/com/polka/android/data/database/model/CollectionItemEntity.kt
+++ android/app/src/main/java/com/polka/android/data/database/model/CollectionItemEntity.kt
@@ -2,43 +2,31 @@ package com.polka.android.data.database.model
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
-import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.polka.android.data.image.ImageSource
+import com.polka.android.data.model.CollectionItem
 
 @Entity(
     tableName = "collection_items",
-    foreignKeys = [
-        ForeignKey(
-            entity = UserEntity::class,
-            parentColumns = ["id"],
-            childColumns = ["ownerId"],
-            onDelete = ForeignKey.CASCADE
-        ),
-
-        ForeignKey(
-            entity = GameEntity::class,
-            parentColumns = ["id"],
-            childColumns = ["gameId"],
-            onDelete = ForeignKey.CASCADE
-        )
-    ],
-
-    indices = [
-        Index("ownerId"),
-        Index("gameId")
-    ]
+    foreignKeys = [ForeignKey(
+        entity = GameEntity::class,
+        parentColumns = ["id"],
+        childColumns = ["gameId"],
+        onDelete = ForeignKey.CASCADE
+    )],
 )
 data class CollectionItemEntity(
-    @PrimaryKey
-    val gameId: Long,
+    @PrimaryKey val gameId: Long,
+    /**
+     * Fractional, so that on order update, we can quickly move it to `(aboveId.displayOrder + belowId.displayOrder)/2`
+     */
+    val displayOrder: Double,
 
-    val status: String? = null,
-    val note: String? = null,
-
-    val userRating: Double? = null,
-
-    val displayOrder: Int = 0,
+    val note: String,
+    val rating: Int?,
+    val status: CollectionItem.Status,
+    val images: List<ImageSource.Saved>,
 
     val createdAt: Long = System.currentTimeMillis(),
-    val updatedAt: Long = System.currentTimeMillis()
+    val updatedAt: Long = System.currentTimeMillis(),
 )
--- android/app/src/main/java/com/polka/android/data/model/CollectionItem.kt
+++ android/app/src/main/java/com/polka/android/data/model/CollectionItem.kt
@@ -1,11 +1,45 @@
 package com.polka.android.data.model
 
+import com.polka.android.data.database.model.CollectionItemEntity
 import com.polka.android.data.image.ImageSource
+import com.polka.android.data.model.CollectionItem.Status
 
 data class CollectionItem(
     val gameId: Long,
-    val rating: Int? = null,
-    val images: List<ImageSource> = listOf(),
-    val notes: String = "",
-    val displayOrder: Int,
-)
+    val displayOrder: Double,
+    val note: String,
+    val rating: Int?,
+    val status: Status,
+    val images: List<ImageSource>,
+) {
+    enum class Status {
+        NO_STATUS, MUST_HAVE, LOVE_TO_HAVE, LIKE_TO_HAVE, THINKING, DO_NOT_BUY;
+
+        override fun toString(): String {
+            return when (this) {
+                NO_STATUS -> "-"
+                MUST_HAVE -> "Must have"
+                LOVE_TO_HAVE -> "Love to have"
+                LIKE_TO_HAVE -> "Like to have"
+                THINKING -> "Thinking about it"
+                DO_NOT_BUY -> "Don't buy this"
+            }
+        }
+    }
+
+    constructor(entity: CollectionItemEntity) : this(
+        entity.gameId, entity.displayOrder, entity.note, entity.rating, entity.status, entity.images
+    )
+}
+
+fun String.toCollectionItemStatus(): Status? {
+    return when (this) {
+        "-" -> Status.NO_STATUS
+        "Must have" -> Status.MUST_HAVE
+        "Love to have" -> Status.LOVE_TO_HAVE
+        "Like to have" -> Status.LIKE_TO_HAVE
+        "Thinking about it" -> Status.THINKING
+        "Don't buy this" -> Status.DO_NOT_BUY
+        else -> null
+    }
+}
