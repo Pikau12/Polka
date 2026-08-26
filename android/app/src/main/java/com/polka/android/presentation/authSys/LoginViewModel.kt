971d389862449e86073337ab555fd67845e39ab2
Dragli <65397329+Draglil@users.noreply.github.com>
2026-08-26 19:34:41 +0700

    feat: update LoginViewModel

--- android/app/src/main/java/com/polka/android/presentation/authSys/LoginViewModel.kt
+++ android/app/src/main/java/com/polka/android/presentation/authSys/LoginViewModel.kt
@@ -1,5 +1,16 @@
 package com.polka.android.presentation.authSys
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
 data class LoginState (
     val loginString: String = "",
     val passwordString: String = "",
@@ -12,8 +23,8 @@ sealed class LoginScreenEvent {
     object onToSignUpScreenNav: LoginScreenEvent()
 
     object onLogInClick: LoginScreenEvent()
-    object onLoginChange: LoginScreenEvent()
-    object onPasswordChange: LoginScreenEvent()
+    data class onLoginChange(val login: String): LoginScreenEvent()
+    data class onPasswordChange(val password: String): LoginScreenEvent()
 }
 
 class LoginViewModel : ViewModel() {
@@ -29,9 +40,9 @@ class LoginViewModel : ViewModel() {
             is LoginScreenEvent.onToSignUpScreenNav -> handleToSignUpScreenNav()
             is LoginScreenEvent.onToOverviewScreenNav -> handleToOverviewScreenNav()
 
-            is LoginScreenEvent.onLogInClick -> handleOnLoginChange()
-            is LoginScreenEvent.onLoginChange -> handleOnLoginChange()
-            is LoginScreenEvent.onPasswordChange -> handleOnPasswordChange()
+            is LoginScreenEvent.onLogInClick -> handleOnLogInClick()
+            is LoginScreenEvent.onLoginChange -> handleOnLoginChange(event.login)
+            is LoginScreenEvent.onPasswordChange -> handleOnPasswordChange(event.password)
         }
     }
 
@@ -39,12 +50,16 @@ class LoginViewModel : ViewModel() {
 
     }
 
-    private fun handleOnLoginChange() {
-
+    private fun handleOnLoginChange(login: String) {
+        _state.update { it.copy(
+            loginString = login
+        ) }
     }
 
-    private fun handleOnPasswordChange() {
-
+    private fun handleOnPasswordChange(password: String) {
+        _state.update { it.copy(
+            passwordString = password
+        ) }
     }
 
     private fun handleToOverviewScreenNav() {
