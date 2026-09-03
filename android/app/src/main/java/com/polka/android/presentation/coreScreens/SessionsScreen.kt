8413f8bd39b9980e64a17680253a7b0ab3039043
Draglil <65397329+Draglil@users.noreply.github.com>
2026-09-04 00:20:54 +0700

    feat: update SessionsScreen and SessionsViewModel

--- android/app/src/main/java/com/polka/android/presentation/coreScreens/SessionsScreen.kt
+++ android/app/src/main/java/com/polka/android/presentation/coreScreens/SessionsScreen.kt
@@ -1,10 +1,13 @@
 package com.polka.android.presentation.coreScreens
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.polka.android.data.model.Session
+import com.polka.android.presentation.navigation.Destination
 
 @Composable
 fun SessionsScreen (
@@ -13,5 +16,23 @@ fun SessionsScreen (
 ) {
     val state by viewModel.state.collectAsState()
 
-
+    LaunchedEffect(Unit) {
+        viewModel.sessionsScreenEvent.collect { event ->
+            when(event) {
+                is SessionsScreenEvent.onSessionClick -> {
+                    navController.navigate(Destination.Session.pass(event.sessionId))
+                }
+                is SessionsScreenEvent.onRightSwipe -> {
+                    navController.navigate(Destination.CollectionCore.route)
+                }
+                is SessionsScreenEvent.onLeftSwipe -> {
+                    navController.navigate(Destination.User.route)
+                }
+                is SessionsScreenEvent.onAddSessionClick -> {
+                    navController.navigate(Destination.SessionCard.pass(event.gameId))
+                }
+                else -> {}
+            }
+        }
+    }
 }
--- android/app/src/main/java/com/polka/android/presentation/coreScreens/SessionsViewModel.kt
+++ android/app/src/main/java/com/polka/android/presentation/coreScreens/SessionsViewModel.kt
@@ -1,25 +1,68 @@
 package com.polka.android.presentation.coreScreens
 
-import androidx.compose.runtime.remember
 import androidx.lifecycle.ViewModel
-import com.polka.android.data.model.Session
+import androidx.lifecycle.viewModelScope
+import com.polka.android.presentation.model.SessionSummary
 import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
 data class SessionsState (
     val isLoading: Boolean = false,
-    val sessions: List<Session>? = null,
+    val sessions: List<SessionSummary>? = null,
 )
 
 sealed class SessionsScreenEvent {
-    data class onSessionClick(val session)
+    object onLeftSwipe : SessionsScreenEvent()
+    object onRightSwipe : SessionsScreenEvent()
+    data class onSessionClick(val sessionId: Long) : SessionsScreenEvent()
+    data class onAddSessionClick(val gameId: Long) : SessionsScreenEvent()
 }
 
 class SessionsViewModel @Inject constructor(
 
-) : ViewModel() {
+) : ViewModel() { // TODO add sort
     private val _state = MutableStateFlow(SessionsState())
     val state : StateFlow<SessionsState> = _state.asStateFlow()
+
+    private val _sessionsScreenEvent = MutableSharedFlow<SessionsScreenEvent>()
+    val sessionsScreenEvent: SharedFlow<SessionsScreenEvent> = _sessionsScreenEvent.asSharedFlow()
+
+    fun handleEvent(event: SessionsScreenEvent) {
+        when(event) {
+            is SessionsScreenEvent.onLeftSwipe -> handleOnLeftSwipe()
+            is SessionsScreenEvent.onRightSwipe -> handleOnRightSwipe()
+            is SessionsScreenEvent.onSessionClick -> handleOnSessionClick(event.sessionId)
+            is SessionsScreenEvent.onAddSessionClick -> handleOnAddSessionClick(event.gameId)
+        }
+    }
+
+    private fun handleOnAddSessionClick(gameId: Long) {
+        viewModelScope.launch {
+            _sessionsScreenEvent.emit(SessionsScreenEvent.onAddSessionClick(gameId))
+        }
+    }
+
+    private fun handleOnLeftSwipe() {
+        viewModelScope.launch {
+            _sessionsScreenEvent.emit(SessionsScreenEvent.onLeftSwipe)
+        }
+    }
+
+    private fun handleOnRightSwipe() {
+        viewModelScope.launch {
+            _sessionsScreenEvent.emit(SessionsScreenEvent.onRightSwipe)
+        }
+    }
+
+    private fun handleOnSessionClick(sessionId: Long) {
+        viewModelScope.launch {
+            _sessionsScreenEvent.emit(SessionsScreenEvent.onSessionClick(sessionId))
+        }
+    }
 }
