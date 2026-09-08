92a1ee63a48e165a78837d505e9e8bed33c1aec5
Draglil <65397329+Draglil@users.noreply.github.com>
2026-09-08 12:12:39 +0700

    refactor: optimize imports

--- android/app/src/main/java/com/polka/android/presentation/common/inputs/SearchWithoutBody.kt
+++ android/app/src/main/java/com/polka/android/presentation/common/inputs/SearchWithoutBody.kt
@@ -1,6 +1,5 @@
 package com.polka.android.presentation.common.inputs
 
-import android.R
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -10,7 +9,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
