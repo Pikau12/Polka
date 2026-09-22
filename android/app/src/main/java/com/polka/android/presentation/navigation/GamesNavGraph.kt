ed85fd79a5ef5f55b9d17381bc25882979128af1
Draglil <65397329+Draglil@users.noreply.github.com>
2026-09-22 17:47:43 +0700

    feat: update GamesNavGraph

--- android/app/src/main/java/com/polka/android/presentation/navigation/GamesNavGraph.kt
+++ android/app/src/main/java/com/polka/android/presentation/navigation/GamesNavGraph.kt
@@ -2,9 +2,35 @@ package com.polka.android.presentation.navigation
 
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import androidx.navigation.navigation
+import com.polka.android.presentation.coreScreens.CollectionScreen
+import com.polka.android.presentation.coreScreens.SessionsScreen
+import com.polka.android.presentation.gamesSys.GameCardScreen
+import com.polka.android.presentation.gamesSys.GameScreen
+import com.polka.android.presentation.gamesSys.GamesSearchScreen
 
 fun NavGraphBuilder.gamesNavGraph(
     navController: NavController
 ){
+    navigation(
+        startDestination = Destination.CollectionGames.route,
+        route = "core"
+    ) {
+        composable(Destination.CollectionGames.route) {
+            CollectionScreen(navController = navController)
+        }
 
+        composable(Destination.Game.route) {
+            GameScreen(navController = navController)
+        }
+
+        composable(Destination.GameCard.route) {
+            GameCardScreen(navController = navController)
+        }
+
+        composable(Destination.GamesSearch.route) {
+            GamesSearchScreen(navController = navController)
+        }
+    }
 }
